140f09e8677a6037df84b5a38d0e8688f1f7bf6e
FRNS1 <fernandesjoao2009@hotmail.com>
2023-11-15 00:00:52 -0300

    up

--- carrosfront/main/templates/homepage/homepage.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    // Seu código JavaScript aqui
-    alert('Script carregado!');
-});
-
-function teste(){
-    alert('porra');
-}
